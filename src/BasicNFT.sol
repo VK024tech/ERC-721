cccb1fed1e88f8cff1f4846f5b6afc31becc4b73
Vivek <kandarivivek260@gmail.com>
2026-01-16 18:03:06 +0000

    min nft

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -6,16 +6,21 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNFT is ERC721 {
     uint256 private s_tokenCounter;
-    constructor() ERC721("Dogie", "Dog"){
+    mapping(uint256 => string) private s_tokenIdToUri;
+
+    constructor() ERC721("Dogie", "Dog") {
         s_tokenCounter = 0;
     }
 
-    function mintNFT() public {
-
+    function mintNFT(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns(string memory){
-        return 
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        return s_tokenIdToUri[tokenId];
     }
-
 }
